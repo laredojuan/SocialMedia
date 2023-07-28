07e20b20412e88fb081c8a3d89a9fcc2d2a034bd
laredojuan <laredo.juan@yahoo.com>
2023-07-28 12:58:04 -0700

    added register

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,105 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import { DASHBOARD, LOGIN } from "lib/routes";
+import { Link as RouterLink } from "react-router-dom";
+import { useRegister } from "hooks/auth";
+import { useForm } from "react-hook-form";
+import { emailValidate, passwordValidate, usernameValidate } from "utils/form-validate";
 
 export default function Register() {
+  const { register: signup, isLoading } = useRegister();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  async function handleRegister(data) {
+    signup({
+      username: data.username,
+      email: data.email,
+      password: data.password,
+      redirectTo: DASHBOARD,
+    });
+  }
+
   return (
-    <div>Register</div>
-  )
+    <Center w="100%" h="100vh">
+      <Box mx="1" maxw="md" p="9" borderWidth="1px" borderRadius="lg">
+        <Heading mb="4" size="lg" textAlign="center">
+          Create Account
+        </Heading>
+        <form onSubmit={handleSubmit(handleRegister)}>
+          <FormControl isInvalid={errors.username} py="2">
+            <FormLabel>Username</FormLabel>
+            <Input
+              placeholder="username"
+              {...register("username", usernameValidate)}
+            />
+            <FormErrorMessage>
+              {errors.username && errors.username.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.email} py="2">
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="email"
+              placeholder="Email"
+              {...register("email", emailValidate)}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.password} py="2">
+            <FormLabel>Password</FormLabel>
+            <Input
+              type="password"
+              placeholder="Password"
+              {...register("password", passwordValidate)}
+            />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button
+            colorScheme="blue"
+            mt="4"
+            type="submit"
+            size="md"
+            w="full"
+            isLoading={isLoading}
+            loadingText="Creating Account"
+          >
+            Register
+          </Button>
+        </form>
+
+        <Text mt="6" fontSize="xlg" textAlign="center">
+          Already have an account?{" "}
+          <Link
+            as={RouterLink}
+            to={LOGIN}
+            color="teal.800"
+            fontWeight="medium"
+            textDecor="underline"
+            _hover={{ background: "teal.100" }}
+          >
+            Register
+          </Link>{" "}
+          Instead
+        </Text>
+      </Box>
+    </Center>
+  );
 }
